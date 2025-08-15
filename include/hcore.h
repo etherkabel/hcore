f6791287e0b3f9092ec69d2a957bfb01eafc88e1
etherkabel <>
2025-08-15 23:17:45 +0300

    Added LIM. LIM (Load in Memory) is a modification in the project for loading a program into RAM without compilation. Fixed the code. The index register is now
    Enable LIM using macros in the project
    
    src/hcore.h
    
    "#define _LIM"
    
    and using a variable in the Makefile
    
    LIM_ENABLE = 1
    
    next, in src/lim.c, write the code line by line in the lim function, loading instructions and arguments into the RAM array
    
    mem[0] = MOV;
    mem[1] = 1; // 1 - this is the index of the register AX
    mem[2] = 2;

--- include/hcore.h
+++ include/hcore.h
@@ -2,6 +2,7 @@
 #define _HCORE_H_INCLUDE_
 #endif
 
+#include <stdio.h>
 #include <stdint.h>
 
 #define ADD 0x1
@@ -10,8 +11,11 @@
 #define NOP 0x4
 #define PUSH 0x5
 #define POP 0x6
+#define EXIT 0x7
 
 #define MEMSIZE 512
+
+#define _LIM
 
 typedef struct {
     int16_t AX;
@@ -27,3 +31,8 @@ typedef struct {
 extern void clsmem(int16_t* mem);
 extern void logging(REGISTER* reg);
 extern int16_t* getRegisterPtr(REGISTER* reg, int16_t index);
+extern void warexit(FILE** rom_file,REGISTER *reg,int error_code,int16_t logging_mode);
+
+#ifdef _LIM
+extern void loadinmemory(int16_t* mem);
+#endif
--- src/hcore.c
+++ src/hcore.c
@@ -7,15 +7,17 @@ int16_t* getRegisterPtr(REGISTER* reg, int16_t index)
 {
     switch(index)
     {
-        case 0: return &reg->AX;
-        case 1: return &reg->BX;
-        case 2: return &reg->CX;
-        case 3: return &reg->DX;
-        case 4: return &reg->PC;
-        case 5: return &reg->IP;
-        case 6: return &reg->SP;
-        case 7: return &reg->SS;
+        case 1: return &reg->AX;
+        case 2: return &reg->BX;
+        case 3: return &reg->CX;
+        case 4: return &reg->DX;
+        case 5: return &reg->PC;
+        case 6: return &reg->IP;
+        case 7: return &reg->SP;
+        case 8: return &reg->SS;
     }
+
+    return NULL;
 }
 
 void initreg(REGISTER* reg)
@@ -29,12 +31,27 @@ void initreg(REGISTER* reg)
     reg->SP = 0;
 }
 
-int main() {
+int main(int argc, char* argv[]) {
     REGISTER reg;
     int16_t* opcode;
+    FILE *rom_file;
+
+    if (!(argc < 2))
+    {
+        if ((rom_file = fopen(argv[2],"rb")) == NULL)
+        {
+            perror("Error open ROM file");
+            return 1;
+        } else
+        {
+
+        }
+    }
 
     clsmem(mem);
     initreg(&reg);
+
+    loadinmemory(mem);
 
     while (reg.IP < MEMSIZE && mem[reg.IP] != 0)
     {
@@ -49,6 +66,11 @@ int main() {
                 int16_t* reg_ptr = getRegisterPtr(&reg, reg_index);
 
                 if (reg_ptr != NULL) *reg_ptr = value;
+                else
+                {
+                    fprintf(stderr,"Inalid register index!\n");
+                    warexit(&rom_file,&reg,3,0);
+                }
 
                 reg.IP += 3;
                 break;
@@ -63,8 +85,14 @@ int main() {
                 int16_t reg_index1 = mem[reg.IP+1];
                 int16_t reg_index2 = mem[reg.IP+2];
 
-                int16_t* reg_ptr1 = getRegisterPtr(&reg, 0);
-                int16_t* reg_ptr2 = getRegisterPtr(&reg, 1);
+                int16_t* reg_ptr1 = getRegisterPtr(&reg, reg_index1);
+                int16_t* reg_ptr2 = getRegisterPtr(&reg, reg_index2);
+
+                if (!reg_ptr1 || !reg_ptr2)
+                {
+                    fprintf(stderr,"Invalid register index!\n");
+                    warexit(&rom_file,&reg,3,0);
+                }
 
                 *reg_ptr1 = *reg_ptr1 + *reg_ptr2;
 
@@ -76,8 +104,14 @@ int main() {
                 int16_t reg_index1 = mem[reg.IP+1];
                 int16_t reg_index2 = mem[reg.IP+2];
 
-                int16_t* reg_ptr1 = getRegisterPtr(&reg, 0);
-                int16_t* reg_ptr2 = getRegisterPtr(&reg, 1);
+                int16_t* reg_ptr1 = getRegisterPtr(&reg, reg_index1);
+                int16_t* reg_ptr2 = getRegisterPtr(&reg, reg_index2);
+
+                if (!reg_ptr1 || !reg_ptr2)
+                {
+                    fprintf(stderr,"Invalid register index!\n");
+                    warexit(&rom_file,&reg,3,0);
+                }
 
                 *reg_ptr1 = *reg_ptr1 - *reg_ptr2;
 
@@ -90,6 +124,12 @@ int main() {
                 int16_t* reg_ptr = getRegisterPtr(&reg, reg_index);
                 mem[reg.SS+reg.SP] = *reg_ptr;
 
+                if (!reg_ptr)
+                {
+                    fprintf(stderr,"Invalid register index!\n");
+                    warexit(&rom_file,&reg,3,0);
+                }
+
                 reg.SP-=2;
                 reg.IP+=2;
 
@@ -100,6 +140,12 @@ int main() {
                 int16_t reg_index = mem[reg.IP+1];
                 int16_t* reg_ptr = getRegisterPtr(&reg, reg_index);
 
+                if (!reg_ptr)
+                {
+                    fprintf(stderr,"Invalid register index!\n");
+                    warexit(&rom_file,&reg,3,0);
+                }
+
                 reg.SP += 2;
                 *reg_ptr = mem[reg.SS+reg.SP];
                 mem[reg.SS+reg.SP] = 0;
@@ -108,14 +154,18 @@ int main() {
 
                 break;
             }
+            case EXIT:
+            {
+                warexit(&rom_file,&reg,reg.DX,1);
+            }
             default:
                 printf("Incorrect opcode - %d at IP=%d\n",*opcode,reg.IP);
-                return 2;
+                warexit(&rom_file,&reg,2,0);
         }
     }
 
-    logging(&reg);
-
-    return 0;
-
+    if (rom_file != NULL)
+        warexit(&rom_file,&reg,0,1);
+    else
+        warexit(NULL,&reg,0,1);
 }
